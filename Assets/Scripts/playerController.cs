8bd33e7d138343e25deab65161a407b8411f0ab2
Dom Digeno <DominicDigeno@gmail.com>
2022-09-08 01:27:48 -0400

    Added Prefab walls and platforms

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -89,7 +89,7 @@ public class playerController : MonoBehaviour, IDamageable
 
     public void respawn()
     {
-        controller.enabled = false;
+        /*controller.enabled = false;*/
         HP = HPOrig;
         updatePlayerHP();
         transform.position = gameManager.instance.playerSpawnPos.transform.position;
