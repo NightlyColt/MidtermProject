6cb15b17c97517314e937d27f28667884325d48d
Micah <mcmillian_micah10895@yahoo.com>
2022-09-21 23:02:15 -0400

    updated gun prefabs

--- Assets/Scripts/RotateObject.cs
+++ Assets/Scripts/RotateObject.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class RotateObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] float rotationSpeed_X, rotationSpeed_Y, rotationSpeed_Z;
     void Update()
     {
-        
+        transform.Rotate(new Vector3(rotationSpeed_X, rotationSpeed_Y, rotationSpeed_Z) * Time.deltaTime);
     }
 }
