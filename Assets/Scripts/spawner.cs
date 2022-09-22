671eaec2af6cdad88c59cdff8412c2a017a2583f
Joshua Wolf <jdwolf@student.fullsail.edu>
2022-09-21 22:33:19 -0400

    Level tweaks, spawner added, player spawn return

---
+++ Assets/Scripts/spawner.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemy;
+    [SerializeField] int maxEnemies;
+    [SerializeField] int timer;
+    int enemiesSpawned;
+    bool isSpawning;
+    bool startSpawning;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (startSpawning)
+        {
+            StartCoroutine(spawn());
+        }
+    }
+
+    IEnumerator spawn()
+    {
+
+        if (!isSpawning && enemiesSpawned < maxEnemies)
+        {
+            isSpawning = true;
+            enemiesSpawned++;
+
+            Instantiate(enemy, transform.position, enemy.transform.rotation);
+
+            yield return new WaitForSeconds(timer);
+            isSpawning = false;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            startSpawning = true;
+        }
+    }
+}
